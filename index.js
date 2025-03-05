6064cd22ab083280270322d737d80873526f06cb
happiness <happinesskhalan@gmail.com>
2025-03-06 00:51:17 +0300

    adds anothers functions concept

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun("roller-skate"))
+
+const mondayWork = function(activity = "go to the office"){
+
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol = "*"){
+
+    return InnerWrap = function(message = "special"){
+        return `You are ${symbol}${message}${symbol}!`
+    }
+
+    
+}
+
+let emphasize =  wrapAdjective("*")
+console.log(emphasize("a hard worker"))
